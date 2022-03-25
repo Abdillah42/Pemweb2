a63c4f9bc91a50a334e2ba4aea6a891d45906b48
Abdillah <abdiker42@gmail.com>
2022-03-25 15:21:29 +0700

    Update connection.php

--- util/connection.php
+++ util/connection.php
@@ -4,7 +4,7 @@ session_start();
 $host = "host = rosie.db.elephantsql.com ";
 $port = "port = 5432";
 $dbname = "dbname = qwnnuslh";
-$username = "username = qwnnuslh";
+$username = "user = qwnnuslh";
 $password = "password = yvrSLjdqCk1EAfnCmAyZcGWzIV7Hw4Xb";
 
 $connection = pg_connect("$host $port $dbname $username $password");
