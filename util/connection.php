3addef6c433818ba85c7742e2b807687bcde4d85
Abdillah <abdiker42@gmail.com>
2022-03-25 15:00:42 +0700

    Update connection.php

--- util/connection.php
+++ util/connection.php
@@ -2,8 +2,8 @@
 
 session_start();
 $host = "rosie.db.elephantsql.com ";
-$port = "5432";
-$dbname = "tb_mahasiswa";
+$port = "port = 5432";
+$dbname = "dbname = tb_mahasiswa";
 $username = "qwnnuslh";
 $password = "yvrSLjdqCk1EAfnCmAyZcGWzIV7Hw4Xb";
 
