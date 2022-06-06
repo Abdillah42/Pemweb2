86773d25b8f94336fdc6b9a48d1369b61013fb8a
Abdillah <abdiker42@gmail.com>
2022-06-06 16:04:21 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -3,8 +3,8 @@
 include('util/connection.php');
 $statement = pg_query($connection, "SELECT * FROM tb_mahasiswa WHERE id=".$_GET['id']);
 while ($row = pg_fetch_array($statement)) {
-    $nim = $row['nim'];
-    $nama = $row['nama'];
+    $nim = $row['Type'];
+    $nama = $row['Tahun Produksi'];
 }
 
 ?>
@@ -20,17 +20,17 @@ while ($row = pg_fetch_array($statement)) {
         
         <div class="container" style="margin-top: 100px; margin-bottom: 100px;">
             <div class="pt-5">
-                <h3 class="text-center"><b>Detail Mahasiswa</b></h3>
+                <h3 class="text-center"><b>Data HP</b></h3>
             </div>
             <div class="card mt-5">
                 <form>
                     <div class="card-body">
                         <div class="form-group">
-                            <label for="nim">NIM</label>
+                            <label for="nim">Type</label>
                             <input type="number" class="form-control" id="nim" name="nim" value="<?php echo $nim; ?>" disabled>
                         </div>
                         <div class="form-group">
-                            <label for="nama">Nama</label>
+                            <label for="nama">Tahun Produksi</label>
                             <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $nama; ?>" disabled>
                         </div>
                     </div>
