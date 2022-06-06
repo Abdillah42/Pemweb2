672acdde07973c2d6f8c9cadce38c8b272015eb2
Abdillah <abdiker42@gmail.com>
2022-06-06 20:21:46 +0700

    Update ubah_proses.php

--- process/ubah_proses.php
+++ process/ubah_proses.php
@@ -9,14 +9,8 @@ if (isset($_POST['submit'])) {
     $tahun = $_POST['tahun'];
     $sql = "UPDATE tb_hp2 SET merk='$merk', type='$type', tahun='$tahun', WHERE id='$id'";
     $result = pg_affected_rows(pg_query($sql));
-     if($result == 1) {
-        $_SESSION['message'] = '<div class="alert alert-success" role="alert">Berhasil Mengubah Data</div>';
-        header("location:../index.php");
-    }
-    else {
-        $_SESSION['message'] = '<div class="alert alert-danger" role="alert">Gagal Mengubah Data</div>';
-        header("location:../ubah.php?id=$id");  
-    }
+    
+    header("location:index.php?pesan=update");
 }
 
 ?>
