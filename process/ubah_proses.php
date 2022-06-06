e430101f8d346657f689f5de8fe77f54490b8e18
Abdillah <abdiker42@gmail.com>
2022-06-06 18:46:01 +0700

    Update ubah_proses.php

--- process/ubah_proses.php
+++ process/ubah_proses.php
@@ -1,17 +1,24 @@
 <?php
 
 include('../util/connection.php');
+$sql = "select * from tb_hp2 where id='" . $_GET['id'] . "'";
+$result = pg_query($sql);
+$data = pg_fetch_object($result);
 
-if (isset($_POST['submit'])) {
+if(isset($_POST['id']) and !empty($_POST['id'])) {
     $id = $_POST['id'];
     $merk = $_POST['merk'];
     $type = $_POST['type'];
     $tahun = $_POST['tahun'];
-    $sql = "UPDATE tb_hp2 SET merk='$merk', type='$type', tahun='$tahun' WHERE id='$id'";
+    $sql = "update tb_hp2 set merk='" . $_POST['merk'] . "', type='" . $_POST['type'] . "', tahun='" .   $_POST['tahun'] . "'"
+        "where id='" . $_POST['id'] . "'";
     $result = pg_affected_rows(pg_query($sql));
-    if($result) {
-        $_SESSION['message'] = '<div class="alert alert-success" role="alert">Perubahan data telah tersimpan</div>';
-        header("location:../index.php");
+    
+    if($result == 1) {
+        echo '<script type="text/javascript">';
+        echo 'alert("Perubahan telah tersimpan");';
+        echo 'window.location.href = "index.php";';
+        echo '</script>';
     }
     else {
         $_SESSION['message'] = '<div class="alert alert-danger" role="alert">Gagal Mengubah Data</div>';
