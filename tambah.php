dd54c5115ff9ea117ed3bf124ae1daf09e6d0bda
Abdillah <abdiker42@gmail.com>
2022-06-06 16:22:27 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -15,7 +15,7 @@ include('util/connection.php');
 
         <div class="container" style="margin-top: 100px; margin-bottom: 100px;">
             <div class="pt-5">
-                <h3 class="text-center"><b>Tambah Mahasiswa</b></h3>
+                <h3 class="text-center"><b>Tambah Data</b></h3>
                 <?php if(!empty($_SESSION['message'])){
                     echo $_SESSION['message'];
                     $_SESSION['message'] = null;
@@ -25,12 +25,16 @@ include('util/connection.php');
                 <form action="process/tambah_proses.php" method="POST">
                     <div class="card-body">
                         <div class="form-group">
-                            <label for="nim">NIM</label>
-                            <input type="number" class="form-control" id="nim" name="nim" placeholder="Masukan NIM..." required>
+                            <label for="nim">Merk Hp</label>
+                            <input type="number" class="form-control" id="merk" name="merk" placeholder="Masukan Merk..." required>
                         </div>
                         <div class="form-group">
-                            <label for="nama">Nama</label>
-                            <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukan Nama..." required>
+                            <label for="nim">Type</label>
+                            <input type="number" class="form-control" id="type" name="type" placeholder="Masukan Type..." required>
+                        </div>
+                        <div class="form-group">
+                            <label for="nama">Tahun Produksi</label>
+                            <input type="text" class="form-control" id="tahun" name="tahun" placeholder="Masukan Tahun..." required>
                         </div>  
                     <div class="card-footer text-right">
                         <button class="btn btn-danger mr-3" type="button" onclick="history.back()">Batal</button>
