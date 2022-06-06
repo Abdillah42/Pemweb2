ac9a45220bc3737ef49e8d34c74f894597282d81
Abdillah <abdiker42@gmail.com>
2022-06-06 16:48:10 +0700

    Update ubah_proses.php

--- process/ubah_proses.php
+++ process/ubah_proses.php
@@ -2,11 +2,11 @@
 
 include('../util/connection.php');
 
-if (isset($_POST['nim']) and !empty($_POST['nim'])) {
-    $id = $_POST['id'];
-    $nim = $_POST['nim'];
-    $nama = $_POST['nama'];
-    $sql = "UPDATE tb_mahasiswa SET nim='$nim', nama='$nama' WHERE id='$id'";
+if (isset($_POST['merk']) and !empty($_POST['merk'])) {
+    $merk = $_POST['Merk']
+    $type = $_POST['type'];
+    $tahun = $_POST['Tahun Produksi'];
+    $sql = "UPDATE tb_hp SET merk='$merk', type='$type', tahun='$tahun' WHERE id='$id'";
     $result = pg_affected_rows(pg_query($sql));
     if($result == 1) {
         echo '<script type="text/javascript">';
