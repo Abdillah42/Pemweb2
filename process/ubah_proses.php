1fec419e3ef7e2c1d78921053b07f2ecbc5d637c
Abdillah <abdiker42@gmail.com>
2022-06-06 18:19:13 +0700

    Update ubah_proses.php

--- process/ubah_proses.php
+++ process/ubah_proses.php
@@ -10,10 +10,8 @@ if (isset($_POST['merk']) and !empty($_POST['merk'])) {
     $sql = "UPDATE tb_hp2 SET merk='$merk', type='$type', tahun='$tahun' WHERE id='$id'";
     $result = pg_affected_rows(pg_query($sql));
     if($result == 1) {
-        echo '<script type="text/javascript">';
-        echo 'alert("Perubahan data telah tersimpan");';
-        echo 'window.location.href="../index.php";';
-        echo "</script>";
+        $_SESSION['message'] = '<div class="alert alert-success" role="alert">Perubahan data telah tersimpan</div>';
+        header("location:../index.php");
     }
     else {
         $_SESSION['message'] = '<div class="alert alert-danger" role="alert">Gagal Mengubah Data</div>';
