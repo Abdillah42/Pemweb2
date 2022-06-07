23f5e296054f1618cd0d96eb9ea897e1e2dce989
Abdillah <abdiker42@gmail.com>
2022-06-07 13:11:04 +0700

    Update ubah_proses.php

--- process/ubah_proses.php
+++ process/ubah_proses.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $merk = $_POST['merk'];
     $type = $_POST['type'];
     $tahun = $_POST['tahun'];
-    $sql = "UPDATE tb_hp2 SET merk='$merk', type='$type', tahun='$tahun', WHERE id='$id'";
+    $sql = "UPDATE tb_hp2 SET merk='$merk', type='$type', tahun='$tahun' WHERE id='$id'";
     $result = pg_affected_rows(pg_query($sql));
     
     if($result == 1) {
